1799d1c7eebc380dc4c64c553d6122d72c36f9f6
Anthoy Aveiga <anthony.aveiga.tic@gmail.com>
2021-07-05 13:52:48 -0500

    route

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,11 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  target: 'static',
   docs: {
     primaryColor: '#E24F55'
-  }
+  },
+  router: 
+  { base: '/markdown_constructors/' }
 })
 
