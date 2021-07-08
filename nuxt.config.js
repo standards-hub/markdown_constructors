dc72eb15f84bef2fa23b33a8af2a51be8c1b7d41
Anthoy Aveiga <anthony.aveiga.tic@gmail.com>
2021-07-08 09:12:12 -0500

    updates in nuxt.config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,18 @@ import theme from '@nuxt/content-theme-docs'
 
 export default theme({
   target: 'static',
+  
+  buildModules: [
+    '@nuxtjs/color-mode'
+  ],
   docs: {
     primaryColor: '#E24F55'
   },
   router: 
-  { base: '/markdown_constructors/' }
+  { base: '/markdown_constructors/' },
+
+  head: {
+    link: [{ rel: 'icon', type: 'image/png', href: './icon.png' }],
+  }
 })
 
